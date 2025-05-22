ad3cb266993523cd3019adcace1675abcad2b2b5
PHIN KIM <phinehasnjuguna1@gmail.com>
2025-05-22 12:58:42 +0300

    cart update

--- src/FinanceTracker.jsx
+++ src/FinanceTracker.jsx
@@ -1,20 +1,19 @@
 import React,{useState,useEffect, useRef,} from "react";
 import MyCharts from "./charts";
 import MyTables from "./tables";
-import { db}  from "./firebase";
-import { collection, addDoc } from "firebase/firestore";
+import { db }  from "./firebase";
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import { serverTimestamp } from "firebase/firestore";
 function FinanceTracker({isDark,onToggleTheme}){
     //USE STATES
     const [searchDisplayed,setSearchDisplayed] = useState(false);
     const [cartDisplayed,setCartDisplay] = useState(false);
     const [selectedMonth,setSelectedMonth] = useState("");
-    //const [budget,setSum]=useState([{col1: "", col2: "", col3: ""},]);
     const [allRows,setAllRows] = useState({
             income:   [{col1: "", col2: "", col3: ""}],
             bills:    [{col1: "", col2: "", col3: ""}],
             expenses: [{col1: "", col2: "", col3: ""}],
             savings:  [{col1: "", col2: "", col3: ""}],
-            //overflow: {col1: "", col2: "", col3: ""},
     });
     const [delayBudget,setBudgetDelay] = useState({
             incomeColumn2:0,
@@ -25,9 +24,9 @@ function FinanceTracker({isDark,onToggleTheme}){
             billColumn3:0,
             expensesColumn3:0,
             savingsColumn3:0,
-            
     });
-    const [savedStatus,setStatusSaved] = useState(false);    
+    const [savedStatus,setStatusSaved] = useState("");    
+    const [loadData,setLoadData] = useState("");
     //USE REF
     const searchRef = useRef(null);
     const cartRef = useRef(null);
@@ -83,8 +82,6 @@ function FinanceTracker({isDark,onToggleTheme}){
     const incomeLeft = incomeActual - billsActual - expensesActual - savingsActual;
     const incomeSpent = incomeActual - incomeLeft;
 
-   // const [budgetDelay,setBudgetDelays] = useState(0);
-    //const [actualDelay,setActualDelays] = useState(0);
     useEffect(()=>{
         const timeout =  setTimeout(() => {
         calculateBudgetTotals()
@@ -142,8 +139,18 @@ function FinanceTracker({isDark,onToggleTheme}){
         try{
             await addDoc(collection(db,"userinputs"),{
                 allRows,
+                grandActualTotal,
+                grandBudgetTotal,
+                incomeBudget,
+                incomeActual,
+                billsBudget,
+                billsActual,
+                savingsBudget,
+                savingsActual,
+                expensesBudget,
+                expensesActual,
                 selectedMonth,
-                timestamp:new Date()
+                timestamp:serverTimestamp()
             });
             setStatusSaved("Data saved !");
             setTimeout(() => setStatusSaved(""),3000 );
@@ -153,7 +160,33 @@ function FinanceTracker({isDark,onToggleTheme}){
                 setTimeout(() => setStatusSaved(""),3000 );
             }
     }
-    
+    const fetchData =async ()=>{
+        try{
+            const querySnapshot = await getDocs(collection(db,"userinputs"));
+            const data = [];
+            querySnapshot.forEach((doc)=>{
+                data.push({id:doc.id,...doc.data()});
+            });
+            if(data.length > 0){
+                const latest = data
+                    .filter(doc=> doc.timestamp)
+                    .sort((a,b)=>{
+                        const aTime = a.timestamp.toDate ? a.timestamp.toDate().getTime() : new Date(a.timestamp).getTime();
+                        const bTime = b.timestamp.toDate ? b.timestamp.toDate().getTime() : new Date(b.timestamp).getTime();
+                        return bTime - aTime;
+                    })[0];
+            setAllRows(latest.allRows)
+            setSelectedMonth(latest.selectedMonth);
+            setLoadData(latest)
+            }
+            
+            return data;
+        }
+        catch(e){
+            alert("error fetching data", + e.message);
+        }
+    }
+
     //OTHER VARIABLES
     const months =[
         "January", "February", "March", "April", "May", "June",
@@ -192,11 +225,60 @@ function FinanceTracker({isDark,onToggleTheme}){
                 <div className={`cartDisplay `} ref={cartRef}>
                         <h1>CART</h1>
                         <img className="cancelCartIcon" src="../src/assets/cancel.png" alt="" onClick={()=>setCartDisplay(false)}/>
-                        <h2>Your cart is currently empty</h2>
-                        <button>Return to Shop</button>
+                        {loadData ?(<h2></h2>) : (<h2>Your cart is currently empty</h2>)}
+                        {loadData && typeof loadData ==="object" &&(
+                            <div>
+                                <table className="summary">
+                                    <thead>
+                                        <tr>
+                                            <th colSpan={3}>{loadData.selectedMonth}  Summary</th>
+                                        </tr>
+                                        <tr>
+                                            <td>Name</td>
+                                            <td>Budget</td>
+                                            <td>Actual</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Income</td>
+                                            <td>{loadData.incomeBudget}</td>
+                                            <td>{loadData.incomeActual}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Bills</td>
+                                            <td>{loadData.billsBudget}</td>
+                                            <td>{loadData.billsActual}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Expenses</td>
+                                            <td>{loadData.expensesBudget}</td>
+                                            <td>{loadData.expensesActual}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Savings</td>
+                                            <td>{loadData.savingsBudget}</td>
+                                            <td>{loadData.savingsActual}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Sum</td>
+                                            <td>{loadData.grandBudgetTotal}</td>
+                                            <td>{loadData.grandActualTotal}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Time saved:</td>
+                                            <td>{loadData.timestamp && loadData.timestamp.toDate
+                                                ? loadData.timestamp.toDate().toLocaleString()
+                                                : new Date(loadData.timestamp).toLocaleString()}
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        )}
+                        <button onClick={fetchData}>Load Data</button>
                 </div>
             )}
-            
         </header>
 
         <hr />
